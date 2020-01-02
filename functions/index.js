693435c044600fa7995106eed237d19a13b1b94e
Richard Okoye <emmanuel.eze@netopng.com>
2020-01-02 02:46:12 +0100

    add endpoints

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,53 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+
+// get screams
+app.get('/screams', (req, res) => {
+    admin
+       .firestore()
+       .collection('screams')
+       .orderBy('createdAt', 'desc')
+       .get()
+       .then(snapshot => {
+           let screams = [];
+           snapshot.forEach(doc => {
+            screams.push({
+                screamId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
+            });
+           })
+           return res.json(screams);
+       })
+       .catch(err =>console.error(err))
+})
+
+// create screams
+app.post('/scream', (req, res) => {
+    let newScream = {
+        userHandle: req.body.userHandle,
+        body: req.body.body,
+        createdAt: new Date().toISOString()
+    }
+
+    admin
+        .firestore()
+        .collection('screams')
+        .add(newScream)
+        .then(doc => {
+            return res.json({ message: `New Scream ${doc.id} created`});
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Something went wrong'});
+            console.log(err);
+        })
+})
+
+exports.api = functions.https.onRequest(app);
